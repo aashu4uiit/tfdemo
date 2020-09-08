02105b39eb71addaf25c3d6dd5b766cb3851bb20
aashu4uiit <aashu4uiit@yahoo.com>
2020-09-08 22:58:24 +1000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIASZOLKLHKW6JVQ7JS"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
